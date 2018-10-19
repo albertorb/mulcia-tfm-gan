13191898bac97c66f775270e08eef6e4d177ee35
Alberto Rincón Borreguero <arincon1992@gmail.com>
2018-10-19 20:29:57 +0200

    reducing network for memory issues

--- convolutional/prepare.py
+++ convolutional/prepare.py
@@ -73,66 +73,66 @@ def get_model():
   """
   inputs = Input((128, 128,3))
 
-  conv1 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(inputs) # 64 : 4 = 16
+  conv1 = Conv2D(32, 3, padding = 'same', kernel_initializer = 'he_normal')(inputs) # 64 : 4 = 16
   conv1 = LeakyReLU(alpha=0.3)(conv1)
-  conv1 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(conv1)  # ""
+  conv1 = Conv2D(32, 3, padding = 'same', kernel_initializer = 'he_normal')(conv1)  # ""
   conv1 = LeakyReLU(alpha=0.3)(conv1)
   pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-  conv2 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(pool1) # 128 : 4 = 32
+  conv2 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(pool1) # 128 : 4 = 32
   conv2 = LeakyReLU(alpha=0.3)(conv2)
-  conv2 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(conv2) # ""
+  conv2 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(conv2) # ""
   conv2 = LeakyReLU(alpha=0.3)(conv2)
   pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-  conv3 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(pool2)# 256 : 4 = 64
+  conv3 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(pool2)# 256 : 4 = 64
   conv3 = LeakyReLU(alpha=0.3)(conv3)
-  conv3 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(conv3)
+  conv3 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(conv3)
   conv3 = LeakyReLU(alpha=0.3)(conv3)
   pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-  conv4 = Conv2D(512, 3, padding = 'same', kernel_initializer = 'he_normal')(pool3)# 512 : 4 = 128
+  conv4 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(pool3)# 512 : 4 = 128
   conv4 = LeakyReLU(alpha=0.3)(conv4)
-  conv4 = Conv2D(512, 3, padding = 'same', kernel_initializer = 'he_normal')(conv4)
+  conv4 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(conv4)
   conv4 = LeakyReLU(alpha=0.3)(conv4)
   drop4 = Dropout(0.5)(conv4)
   pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-  conv5 = Conv2D(1024, 3,padding = 'same', kernel_initializer = 'he_normal')(pool4)
+  conv5 = Conv2D(512, 3,padding = 'same', kernel_initializer = 'he_normal')(pool4)
   conv5 = LeakyReLU(alpha=0.3)(conv5)
-  conv5 = Conv2D(1024, 3,padding = 'same', kernel_initializer = 'he_normal')(conv5)
+  conv5 = Conv2D(512, 3,padding = 'same', kernel_initializer = 'he_normal')(conv5)
   conv5 = LeakyReLU(alpha=0.3)(conv5)
   drop5 = Dropout(0.5)(conv5)
 
-  up6 = Conv2D(512, 2, padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+  up6 = Conv2D(256, 2, padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
   up6 = LeakyReLU(alpha=0.3)(up6)
   merge6 = merge([drop4,up6], mode = 'concat', concat_axis = 3)
-  conv6 = Conv2D(512, 3, padding = 'same', kernel_initializer = 'he_normal')(merge6)
+  conv6 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(merge6)
   conv6 = LeakyReLU(alpha=0.3)(conv6)
-  conv6 = Conv2D(512, 3, padding = 'same', kernel_initializer = 'he_normal')(conv6)
+  conv6 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(conv6)
   conv6 = LeakyReLU(alpha=0.3)(conv6)
 
-  up7 = Conv2D(256, 2, padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+  up7 = Conv2D(128, 2, padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
   up7 = LeakyReLU(alpha=0.3)(up7)
   merge7 = merge([conv3,up7], mode = 'concat', concat_axis = 3)
-  conv7 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(merge7)
+  conv7 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(merge7)
   conv7 = LeakyReLU(alpha=0.3)(conv7)
-  conv7 = Conv2D(256, 3, padding = 'same', kernel_initializer = 'he_normal')(conv7)
+  conv7 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(conv7)
   conv7 = LeakyReLU(alpha=0.3)(conv7)
 
-  up8 = Conv2D(128, 2,padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+  up8 = Conv2D(64, 2,padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
   up8 = LeakyReLU(alpha=0.3)(up8)
   merge8 = merge([conv2,up8], mode = 'concat', concat_axis = 3)
-  conv8 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(merge8)
+  conv8 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(merge8)
   conv8 = LeakyReLU(alpha=0.3)(conv8)
-  conv8 = Conv2D(128, 3, padding = 'same', kernel_initializer = 'he_normal')(conv8)
+  conv8 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(conv8)
   conv8 = LeakyReLU(alpha=0.3)(conv8)
 
-  up9 = Conv2D(64, 2, padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+  up9 = Conv2D(32, 2, padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
   up9 = LeakyReLU(alpha=0.3)(up9)
   merge9 = merge([conv1,up9], mode = 'concat', concat_axis = 3)
-  conv9 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(merge9)
+  conv9 = Conv2D(32, 3, padding = 'same', kernel_initializer = 'he_normal')(merge9)
   conv9 = LeakyReLU(alpha=0.3)(conv9)
-  conv9 = Conv2D(64, 3, padding = 'same', kernel_initializer = 'he_normal')(conv9)
+  conv9 = Conv2D(32, 3, padding = 'same', kernel_initializer = 'he_normal')(conv9)
   conv9 = LeakyReLU(alpha=0.3)(conv9)
   conv9 = Conv2D(2, 3, padding = 'same', kernel_initializer = 'he_normal')(conv9)
   conv9 = LeakyReLU(alpha=0.3)(conv9)
