7900c944dac756d27320b4bc56aeb03293d0dd76
Alberto Rincón Borreguero <arincon1992@gmail.com>
2018-10-19 20:44:10 +0200

    setting more options as arguments

--- convolutional/train.py
+++ convolutional/train.py
@@ -11,14 +11,21 @@ logging.basicConfig(level=logging.INFO)
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch-size", help="Set the batch size", type=int)
-parser.add_argument("--epochs", help="Set number of epochs", type=int)
+parser.add_argument("--batch-size", help="Tamaño del batch", type=int)
+parser.add_argument("--epochs", help="Número de épocas", type=int)
+parser.add_argument("--resolution", help="Cambiar resolución de las imágenes durante entrenamiento. Sólo escribir una dimensión, ie: 128, para redimensionar a 128x128", type=int)
+parser.add_argument("--train", help="Ruta a las imagenes de entrenamiento ", type=str)
+parser.add_argument("--label", help="Ruta a las etiquetas de entrenamiento", type=str)
+parser.add_argument("--export-dir", help="Ruta donde guardar los pesos generados ", type=str)
+
 args = parser.parse_args()
 
 
 
-Y_train = get_masks('data/stage1/stage1_train/')
-X_train = get_data('TRAIN STAGE 1', 'data/stage1/stage1_train')
+#Y_train = get_masks('data/stage1/stage1_train/', resolution=(args.resolution,args.resolution))
+#X_train = get_data('TRAIN STAGE 1', 'data/stage1/stage1_train', resolution=(args.resolution,args.resolution))
+Y_train = get_masks(args.label, resolution=(args.resolution,args.resolution))
+X_train = get_data(args.train, args.train, resolution=(args.resolution,args.resolution))
 
 model = get_model()
 
