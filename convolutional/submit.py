9e26e952296f23b5f552544e50d2adfd5a452ea9
Alberto Rincón Borreguero <arincon1992@gmail.com>
2018-10-20 13:27:52 +0200

    resolution arg in test v2

--- convolutional/submit.py
+++ convolutional/submit.py
@@ -24,7 +24,7 @@ args = parser.parse_args()
 
 model = load_trained_model(args.model_dir)
 
-test = get_data(args.test_dir,args.test_dir,args.resolution)
+test = get_data(args.test_dir,args.test_dir,resolution=(args.resolution,args.resolution))
 
 logging.info("Realizando predicciones sobre el conjunto de test")
 predictions = model.predict(test)
